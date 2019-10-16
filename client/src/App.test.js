db82da620624099887986feb2c38b76350ec5012
MohammadHasham <muhammad.hasham.2311@gmail.com>
2019-10-16 19:45:13 +0500

    added files

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,3 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {});
+
+//hostname is automatically added in docker-compose but in aws json we have to mention that explictly.
+//by giving a hostname it gets to that container as in nginx default.conf.
+//essential true means that if this container crashes all other container will be shutdown as well.
+//one container should be marked as essential.
+//hostPort is port of machine and containerPort is of container.
+//links are unidirectional to form a connection from nginx to server and client. The server and client
+//are the hostnames of other two containers in this file.
